a99ecdc3160c4c3b185f4d33c0cfae72ec070004
Sultan <skurmanov12@gmail.com>
2021-08-17 17:00:23 +0600

    check

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -14,7 +14,7 @@ using MvcMovie.Data;
 namespace MvcMovie
 {
     public class Startup
-    {
+    {/**/
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
